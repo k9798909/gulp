ae89b6ac6c99f1e50b89849e78633b0e164ad5b3
k9798909 <k9798909@gmail.com>
2021-08-22 02:07:59 +0800

    整理

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,10 @@
-console.log('Hello!');
+let buycar = document.getElementsByClassName("buycar")[0];
+buycar.style = "display:none";
+let buycarEvent = () => {
+    if(buycar.style.display == "none") {
+        buycar.style = "display:show";
+    } else {
+        buycar.style = "display:none";
+    }
+}
 
-$(document).ready(() => {
-  console.log('HesSchool Hello!');
-});
